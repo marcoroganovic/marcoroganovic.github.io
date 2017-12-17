0a0909c57e58d9cd0ebfeef846661b22ad0b87ca
Marco Roganovic <marcoroganovic@gmail.com>
2017-12-17 17:39:02 +0100

    add basic animation for projects area

--- js/projects.js
+++ js/projects.js
@@ -1,4 +1,5 @@
 App.module.define("projects", ["dom"], function(dom) {
+
   var PROJECTS = {
     abago: {
       name: "abago.com",
@@ -19,21 +20,44 @@ App.module.define("projects", ["dom"], function(dom) {
     }
 
   }
-  
+
+  var ACTIVE_PROJECT = "abago";
+
+  // cache DOM elements
+  var $featuredProj = dom.$(".featured-project");
   var $browserMockup = dom.$(".browser-mockup img");
   var $projTitle = dom.$(".featured-project-desc h3");
   var $projDesc = dom.$(".featured-project-desc p");
   var $projURL = dom.$(".featured-project-desc a");
   var $otherProjects = dom.$(".other-projects");
 
+  // animation name
+  var ANIMATION_NAME = "zoomIn";
+
+  function animateFeaturedProject(el) {
+    return new Promise(function(resolve, reject) {
+      el.classList.remove(ANIMATION_NAME);
+      setTimeout(function() {
+        el.classList.add(ANIMATION_NAME);
+        resolve();
+      }, 150);
+    });
+  }
+
   $otherProjects.addEventListener("click", function(event) {
     if(event.target.nodeName.toLowerCase() === "img") {
       var id = event.target.dataset.projectId;
-      var project = PROJECTS[id];
-      $browserMockup.src = event.target.src;
-      $projTitle.textContent = project.name;
-      $projDesc.textContent = project.description;
-      $projURL.href = project.link; 
+      if(id !== ACTIVE_PROJECT) {
+        var project = PROJECTS[id];
+        ACTIVE_PROJECT = id;
+
+        animateFeaturedProject($featuredProj).then(function() {
+          $browserMockup.src = event.target.src;
+          $projTitle.textContent = project.name;
+          $projDesc.textContent = project.description;
+          $projURL.href = project.link; 
+        });
+      }
     }
   });
 
